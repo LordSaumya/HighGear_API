2ddecad2bfe28e29de72594716bb8561eed5973a
LordSaumya <e1036312@u.nus.edu>
2023-01-17 00:53:41 +0800

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #Routes
+  get 'users/s/:username', to: "users#search"
+  get 'users/:username/comments', to: "users#comments"
+  resources :users, only: [:index, :show, :create, :update, :destroy]
+  resources :forum_threads, only: [:index, :show, :create, :update, :destroy] do
+    resources :comments, only: [:index, :show, :create, :update, :destroy]
+  end
 end
